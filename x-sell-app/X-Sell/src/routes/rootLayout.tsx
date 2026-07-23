82241bc4ebbf864c280df94806ade63371c3208e
Leonardo Epiphanio Galvão <leonardo.epgalvao@gmail.com>
2026-07-23 10:22:50 -0300

    single na web + títulos

--- x-sell-app/X-Sell/src/routes/rootLayout.tsx
+++ x-sell-app/X-Sell/src/routes/rootLayout.tsx
@@ -6,19 +6,19 @@ export default function RootLayout() {
 
   return <Stack>
           <Stack.Protected guard={!isLogged}>
-            <Stack.Screen name="login" options={{headerShown: false}}/>
-            <Stack.Screen name="register" options={{headerShown: false}}/>
+            <Stack.Screen name="login" options={{headerShown: false, title: "Entrar"}}/>
+            <Stack.Screen name="register" options={{headerShown: false, title: "Registro"}}/>
           </Stack.Protected>
           
           <Stack.Protected guard={isLogged && !isAdmin}>
-            <Stack.Screen name="(tabs)" options={{headerShown: false}} />
+            <Stack.Screen name="(tabs)" options={{headerShown: false, title: "Home"}} />
           </Stack.Protected>
 
           <Stack.Protected guard={isLogged && isAdmin}>
-            <Stack.Screen name="(admin)" options={{headerShown: false}} />
+            <Stack.Screen name="(admin)" options={{headerShown: false, title: "Área do administrador"}} />
           </Stack.Protected>
 
-          <Stack.Screen name="about" options={{headerShown: false}}/>
+          <Stack.Screen name="about" options={{headerShown: false, title: "Sobre"}}/>
         </Stack>
 }
 
